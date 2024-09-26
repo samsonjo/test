125287296ed32a5ed18358fae9d5606cb1a5b86f
samsonjo <99871963+samsonjo@users.noreply.github.com>
2024-09-26 14:10:25 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,39 @@
-# This program prints Hello, world!
-
-print('Hello, world!')
+pipeline {
+    agent { 
+        node {
+            label 'docker-agent-python'
+            }
+      }
+    triggers {
+        pollSCM '* * * * *'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                echo "Building.."
+                sh '''
+                cd myapp
+                pip install -r requirements.txt
+                '''
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "Testing.."
+                sh '''
+                cd myapp
+                python3 hello.py
+                python3 hello.py --name=Brad
+                '''
+            }
+        }
+        stage('Deliver') {
+            steps {
+                echo 'Deliver....'
+                sh '''
+                echo "doing delivery stuff.."
+                '''
+            }
+        }
+    }
+}
